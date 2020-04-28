cca7d77657e5f88454fa0c9a5b3e2abade489f74
Andre Irawan <andreirawan97@gmail.com>
2020-04-28 18:53:31 +0700

    Add package + Buat UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  // State buat nampung counternya
+  let [counter, setCounter] = useState(0);
+
+  // Function pas pencet aku dipencet
+  let tambahCounter = () => {
+    let newCounter = counter + 1;
+    setCounter(newCounter);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View
+        style={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginBottom: 20,
+        }}
+      >
+        <Text>Counter: </Text>
+        <Text>{counter}</Text>
+      </View>
+
+      <TouchableOpacity
+        onPress={tambahCounter}
+        style={{
+          backgroundColor: '#ed1b2e',
+          padding: 20,
+          width: '85%',
+          borderRadius: 12,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Text style={{ color: 'white', fontWeight: 'bold' }}>Pencet aku</Text>
+      </TouchableOpacity>
     </View>
   );
 }
